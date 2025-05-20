bd01aeb90d573f3ae17946d1bfd5a54a8dcf4eb5
nehorayChalfon <neoray0166@gmail.com>
2025-05-20 15:09:20 +0300

    checking for stop_words

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pickle
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-# Check and download required NLTK data only if not already present
+# Check and download only if not already present
 try:
     nltk.data.find('tokenizers/punkt')
-    nltk.data.find('tokenizers/punkt_tab')
 except LookupError:
     nltk.download('punkt')
-    nltk.download('punkt_tab')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
 
 # Load from file
 with open("newsgroups_data.pkl", "rb") as f:
     newsgroups = pickle.load(f)
 
-
+stop_words = set(stopwords.words('english'))
 first_doc = newsgroups.data[0].lower()
 first_doc_tokenized = word_tokenize(first_doc)
-print(first_doc_tokenized)
+
+doc_tokenized_no_stop_words = [token for token in first_doc_tokenized if token not in stop_words]
+print(doc_tokenized_no_stop_words)
